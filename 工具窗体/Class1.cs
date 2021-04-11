212fdf202e3651db04b8ff402c93d9b334d56547
luu2017 <1052104435@@qq.com>
2021-04-11 22:20:17 +0800

    折腾死了

---
+++ 工具窗体/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warehouse.工具窗体
+{
+    class Class1
+    {
+        string m = "hahahha";
+    }
+}
