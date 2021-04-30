aa89b062ea891b3e833ec6b7801c0021f199f85d
a1445605672 <41555798+a1445605672@users.noreply.github.com>
2021-04-30 09:39:25 +0800

    新增库存控制

---
+++ 库存控制.Designer.cs
@@ -0,0 +1,66 @@
+﻿
+namespace Warehouse
+{
+    partial class 库存控制
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.uiLabel1 = new Sunny.UI.UILabel();
+            this.PagePanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // PagePanel
+            // 
+            this.PagePanel.Controls.Add(this.uiLabel1);
+            // 
+            // uiLabel1
+            // 
+            this.uiLabel1.Font = new System.Drawing.Font("微软雅黑", 12F);
+            this.uiLabel1.Location = new System.Drawing.Point(53, 57);
+            this.uiLabel1.Name = "uiLabel1";
+            this.uiLabel1.Size = new System.Drawing.Size(168, 62);
+            this.uiLabel1.TabIndex = 0;
+            this.uiLabel1.Text = "uiLabel1";
+            this.uiLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // 库存控制
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "库存控制";
+            this.Text = "库存控制";
+            this.PagePanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Sunny.UI.UILabel uiLabel1;
+    }
+}
---
+++ 库存控制.cs
@@ -0,0 +1,25 @@
+﻿using Sunny.UI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Warehouse
+{
+    public partial class 库存控制 : UITitlePage
+    {
+        public 库存控制()
+        {
+            InitializeComponent();
+            //库存控制思路：
+            //1、根据物料ID对于在库物料的重量进行求和
+            //2、构建一个  物料ID-上限-下限表，每一个物料ID都会有上下限
+            //3、如果在库求和  超出或者小于上下限，进行提示
+        }
+    }
+}
