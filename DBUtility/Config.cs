6a7dfbcdd9400649be3c2b0b4edb00642f06b80b
luu2017 <1052104435@qq.com>
2021-05-03 22:32:22 +0800

    调拨

--- DBUtility/Config.cs
+++ DBUtility/Config.cs
@@ -17,10 +17,10 @@ namespace DBUtility
         //Mysql数据库所在文件夹
         public static string Backup_origin = @"D:\DataBase\MySQL Server 5.7\Data\warehouse\";
 
-        //数据备份批处理文件
-        public static string Backup_bat = @"E:\git版本\Warehouse_gra\Backup_And_Recovery\mysql_backup.bat";
+        //数据备份批处理文件 
+        public static string Backup_bat = @"C:\Users\qinrui\Documents\GitHub\Warehouse_gra\Backup_And_Recovery\mysql_backup.bat";
 
         //数据恢复批处理文件
-        public static string Recover_bat = @"E:\git版本\Warehouse_gra\Backup_And_Recovery\Backup_And_Recovery";
+        public static string Recover_bat = @"C:\Users\qinrui\Documents\GitHub\Warehouse_gra\Backup_And_Recovery\mysql_recover.bat";
     }
 }
